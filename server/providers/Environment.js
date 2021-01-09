3b373fc85ebf1888e522fa0219e5cf7f1ea0edfd
Ariel Fisher <fishondor@gmail.com>
2021-01-09 22:38:56 +0200

    Implement defaults for environment vars

--- server/providers/Environment.js
+++ server/providers/Environment.js
@@ -1,7 +1,13 @@
 
 require('dotenv').config({ path: `${process.cwd()}/.env` });
 
-const getEnvVar = name => process.env[name];
+const defaults = require("./defaults.json");
+const getEnvVar = (name) => {
+    let value = process.env[name] || defaults[name];
+    if(typeof value === 'undefined')
+        throw new Error(`Environment variable ${name} was not found`);
+    return value;
+};
 
 module.exports = {
     getEnvVar
